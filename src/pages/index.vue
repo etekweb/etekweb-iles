d7432406bb0a72ecfe9dc100e0b5aef97b552644
Ethan <ethan@etekweb.net>
2022-10-29 11:42:16 -0400

    feat: socials

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,6 +14,21 @@
       </div>
       <img src="@/assets/hero.jpeg" alt="Picture of Ethan Matzdorf standing in the woods" />
     </div>
+    <div class="socials">
+      <p>Find me on:</p>
+      <a href="https://www.facebook.com/ethan.matzdorf" target="_blank">
+        <img src="@/assets/social-logos/facebook.png" alt="Facebook link" class="social-link" />
+      </a>
+      <a href="https://www.instagram.com/ethan_m218" target="_blank">
+        <img src="@/assets/social-logos/instagram.png" alt="Instagram link" class="social-link" />
+      </a>
+      <a href="https://www.linkedin.com/in/ethan-matzdorf-9493b4137" target="_blank">
+        <img src="@/assets/social-logos/linkedin.png" alt="Linkedin link" class="social-link" />
+      </a>
+      <a href="https://github.com/etekweb" target="_blank">
+        <img src="@/assets/social-logos/github.png" alt="GitHub link" class="social-link" />
+      </a>
+    </div>
   </div>
 </template>
 
@@ -41,6 +56,28 @@
   }
   img {
     width: 320px;
+  }
+}
+.socials {
+  background-color: var(--color-divider);
+  padding: 16px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  box-shadow: 0px 0px 9px rgba(213, 213, 213, 0.6);
+  @media (prefers-color-scheme: dark) {
+    box-shadow: 0px 0px 9px rgba(83, 83, 83, 0.6);
+  }
+  .social-link {
+    margin-top: 4px;
+  }
+  p {
+    font-size: 28px;
+    margin-bottom: 2px;
+  }
+  img {
+    height: 70px;
   }
 }
 </style>
