f280249afabdf55c6c8f49d88a40f4ad975f38c1
Ethan Matzdorf <ethan@etekweb.net>
2025-05-24 02:07:07 -0400

    bulk uuid generation

--- src/components/UUIDGeneratorTool.vue
+++ src/components/UUIDGeneratorTool.vue
@@ -1,60 +1,86 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { v4 } from 'uuid';
 
-const uuid = ref('');
+const numBulkUUIDs = ref(1);
+const uuidBulk = ref('');
+const copiedBulk = ref<boolean | null>(null);
+let copiedBulkTimeout: ReturnType<typeof setTimeout>;
 
-const copied = ref<boolean | null>(null);
-let copiedTimeout: ReturnType<typeof setTimeout>;
-
-function generateUUID() {
-    uuid.value = v4();
-    copied.value = null;
-    if (copiedTimeout) {
-        clearTimeout(copiedTimeout);
+const copyBtnText = computed(() => {
+    if (copiedBulk.value === null) {
+        return 'Copy';
+    } else if (copiedBulk.value) {
+        return 'Copied!';
+    } else {
+        return 'Copy Error';
     }
+})
+
+function generateBulkUUIDs() {
+    uuidBulk.value = '';
+    for (let i = 0; i < numBulkUUIDs.value; i++) {
+        uuidBulk.value += `${v4()}\n`;
+    }
+    if (copiedBulkTimeout) {
+        clearTimeout(copiedBulkTimeout);
+    }
+    copiedBulk.value = null;
 }
 
-function copyUUID() {
-    if (uuid.value) {
-        navigator.clipboard.writeText(uuid.value)
+function copyUUIDBulk() {
+    if (uuidBulk.value) {
+        navigator.clipboard.writeText(uuidBulk.value)
             .then(() => {
-                copied.value = true;
+                copiedBulk.value = true;
             })
             .catch(err => {
-                copied.value = false;
+                copiedBulk.value = false;
                 console.error('Failed to copy UUID: ', err);
             })
             .finally(() => {
-                if (copiedTimeout) {
-                    clearTimeout(copiedTimeout);
+                if (copiedBulkTimeout) {
+                    clearTimeout(copiedBulkTimeout);
                 }
-                copiedTimeout = setTimeout(() => {
-                    copied.value = null;
+                copiedBulkTimeout = setTimeout(() => {
+                    copiedBulk.value = null;
                 }, 2000);
             });
     }
 }
+
+
 onMounted(() => {
-    generateUUID();
+    // generateUUID();
+    generateBulkUUIDs();
 });
 </script>
 
 <template>
-  <div class="ip-address-tool">
-    <p>UUID (v4):</p>
-    <h1>{{ uuid }}</h1>
-    <div class="buttons">
-      <button class="btn major" v-if="copied === null" @click="copyUUID">Copy</button>
-      <button class="btn major" v-else disabled>{{ copied ? 'Copied!' : 'Copy Error' }}</button>
-      <button class="btn" @click="generateUUID">Generate New</button>
+  <div class="uuid-generator-tool">
+    <div class="num-uuids">
+        <p>Number of UUIDs to generate (v4):</p>
+        <input type="number" min="1" v-model.number="numBulkUUIDs" />
+        <button class="btn" @click="generateBulkUUIDs">Generate</button>
     </div>
+    <textarea v-model="uuidBulk" rows="14" style="width: 100%;"></textarea>
+    <button class="btn major" @click="copyUUIDBulk">{{ copyBtnText }}</button>
   </div>
 </template>
 
-<style scoped>
-.buttons {
-  display: flex;
-  gap: 8px;
+<style scoped lang="scss">
+.num-uuids {
+    display: flex;
+    gap: 16px;
+    margin: 16px 0;
+    p {
+        margin: 0;
+    }
+    input {
+        width: 100px;
+    }
+}
+textarea {
+    margin-bottom: 16px;
 }
 </style>
