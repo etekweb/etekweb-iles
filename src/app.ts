0e0b4eccef281fea4b306927f7c9bcc370f996fd
Ethan Matzdorf <ethan.matzdorf@24g.com>
2026-04-03 23:19:29 -0400

    add og tags

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { defineApp } from "iles";
+import site from "@/site";
 
 export default defineApp({
   head() {
@@ -7,6 +8,38 @@ export default defineApp({
         {
           name: "viewport",
           content: "width=device-width,initial-scale=1,maximum-scale=1",
+        },
+        {
+          property: "og:title",
+          content: site.title,
+        },
+        {
+          property: "og:description",
+          content: site.description,
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:image",
+          content: "/share-card.jpg",
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:image",
+          content: "/share-card.jpg",
+        },
+        {
+          name: "twitter:title",
+          content: site.title,
+        },
+        {
+          name: "twitter:description",
+          content: site.description,
         },
       ],
     };
